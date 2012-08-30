a32935607b263489404eff7d866f2b0dbc924cf5
Thiago de Freitas <thiagodefreitas@gmail.com>
2012-08-30 09:20:31 +0200

    Starts to change the pre check
    According to the new yaml definition,
    
    Some of the main modifications are:
    * Deleted the ready_check subfunctions as they are going to be implemented using the diagnostics topic
    * joint_configuration_check has no more hard-coded definitions
    * Divided joint_configuration_check  into 3 subfunctions, where one
    consists of the implementation of the check

--- cob_skill_api/src/pre_check.py
+++ cob_skill_api/src/pre_check.py
@@ -58,104 +58,10 @@ class PreConditionCheck(ConditionCheck):
 
 		self.result = "failed"
 
-		self.xy_goal_tolerance  = self.checks['pose_check']['allowed_error_position']
-		self.yaw_goal_tolerance = self.checks['pose_check']['allowed_error_orientation']
-
-	####################################################################
-	# function: init_check()
-	# This function is responsible for checking the initial conditions
-	# provided by the user for proper operation of the Robot
-	#####################################################################
-
-	def init_check(self):
-
-		def check_base():
-			if rospy.has_param('move_base'):
-				self.result = "success"
-			else:
-				self.result = "failed"
-
-			rospy.loginfo("Checking the <<base>> component resulted on %s"%self.result)			
-
-		def check_head():
-
-			if rospy.has_param('head_controler'):
-				self.result = "success"
-			else:
-				self.result = "failed"
-			rospy.loginfo("Checking the <<head>> component resulted on %s"%self.result)
-
-		def check_arm():
-
-			if rospy.has_param('arm_controller'):
-				self.result = "success"
-			else:
-				self.result = "failed"
-
-			rospy.loginfo("Checking the <<arm>> component resulted on %s"%self.result)
+		self.xy_goal_tolerance  = self.checks['pose_check'][0]['allowed_position_error']
+		self.yaw_goal_tolerance = self.checks['pose_check'][0]['allowed_orientation_error']
 
 
-		def check_arm_left():
-
-			if rospy.has_param('arm_left_controller'):
-				self.result = "success"
-			else:
-				self.result = "failed"
-
-			rospy.loginfo("Checking the <<left arm>> component resulted on %s"%self.result)
-
-		def check_arm_right():
-
-			if rospy.has_param('arm_right_controller'):
-				self.result = "success"
-			else:
-				self.result = "failed"
-
-			rospy.loginfo("Checking the <<right arm>> component resulted on %s"%self.result)
-
-
-
-		def check_tray():
-
-			if rospy.has_param('tray_controller'):
-
-				self.result = "success"
-			else:
-				self.result = "failed"
-
-			rospy.loginfo("Checking the <<tray>> component resulted on %s"%self.result)
-
-		def check_torso():
-
-			if rospy.has_param('torso_controller'):
-				self.result = "success"
-			else:
-				self.result = "failed"
-
-			rospy.loginfo("Checking the <<torso>> component resulted on %s"%self.result)
-
-		def check_sdh():
-
-			if rospy.has_param('sdh_controller'):
-				self.result = "success"
-			else:
-				self.result = "failed"
-
-			rospy.loginfo("Checking the <<sdh>> component resulted on %s"%self.result)
-
-		for check in self.full_components:
-
-			try:
-				{'base': check_base, 'tray': check_tray, 'torso': check_torso, 'head': check_head, 'arm': check_arm, \
-					'sdh': check_sdh, 'arm_left': check_arm_left, 'arm_right': check_arm_right}[check]()
-
-				if self.result == "failed":
-					rospy.logerr("Failed when checking component <<%s>>"%check)
-					return
-			except KeyError:
-				pass
-
-		self.init_components()
 
 	#####################################################################
 	# function: joint_configuration_check()
@@ -163,135 +69,69 @@ class PreConditionCheck(ConditionCheck):
 	# Joint configuration of the Robot before performing the skill action
 	#####################################################################
 
-	def joint_configuration_check(self):
+	def joint_configuration_check_js(self): # from parameter server, get names and supposed states
 
+		#get joint_names from parameter server (yaml)
+		#get joint_states from parameter server (yaml)
 
-		def arm_left_joint():
+		return self.joint_configuration_check(joint_names,joint_states)
+ 
 
-			rospy.loginfo("Checking the left <<arm>> joints")
-			arm_left_joints = rospy.get_param("/script_server/arm_left/joint_names")
-			jointsMsg = rospy.wait_for_message("/joint_states", sensor_msgs.msg.JointState)
+	def joint_configuration_check_ss(self):
 
-			while arm_left_joints[0] not in jointsMsg.name:
-				jointsMsg = rospy.wait_for_message("/joint_states", sensor_msgs.msg.JointState)
-			    # message is from arm
+		component = self.checks['joint_configuration_check_ss'][0]['component']
+		configuration = self.checks['joint_configuration_check_ss'][0]['configuration']
 
-			angles = []
-			for name in arm_left_joints:
-				angles.append(jointsMsg.position[jointsMsg.name.index(name)])
+		ss_names_path = "/script_server/" + component + "/joint_names"
+		ss_values_path = "/script_server/" + component + "/" + configuration
 
-			aw_error = self.checks['joint_configuration_check']['arm_left']['allowed_error']
+		joint_names = rospy.get_param(ss_names_path)
+		joint_states = rospy.get_param(ss_values_path)[0]
 
-			assert abs(angles[0] - self.checks['joint_configuration_check']['arm_left']['joint_states'][0]) <= aw_error, "Error on the Left Arm for the 1-DOF"
-			assert abs(angles[1] - self.checks['joint_configuration_check']['arm_left']['joint_states'][1]) <= aw_error, "Error on the Left Arm for the 2-DOF"
-			assert abs(angles[2] - self.checks['joint_configuration_check']['arm_left']['joint_states'][2]) <= aw_error, "Error on the Left Arm for the 3-DOF"
-			assert abs(angles[3] - self.checks['joint_configuration_check']['arm_left']['joint_states'][3]) <= aw_error, "Error on the Left Arm for the 4-DOF"
-			assert abs(angles[4] - self.checks['joint_configuration_check']['arm_left']['joint_states'][4]) <= aw_error, "Error on the Left Arm for the 5-DOF"
-			assert abs(angles[5] - self.checks['joint_configuration_check']['arm_left']['joint_states'][5]) <= aw_error, "Error on the Left Arm for the 6-DOF"
-			assert abs(angles[6] - self.checks['joint_configuration_check']['arm_left']['joint_states'][6]) <= aw_error, "Error on the Left Arm for the 7-DOF"
+		return self.joint_configuration_check(joint_names,joint_states)
 
+	def joint_configuration_check(self, joint_names, joint_states):
 
-		def arm_right_joint():
+		joints = zip(joint_names, joint_states)
 
-			rospy.loginfo("Checking the right <<arm>> joints")
-			arm_right_joints = rospy.get_param("/script_server/arm_right/joint_names")
-			jointsMsg = rospy.wait_for_message("/joint_states", sensor_msgs.msg.JointState)
-
-			while arm_right_joints[0] not in jointsMsg.name:
-				jointsMsg = rospy.wait_for_message("/joint_states", sensor_msgs.msg.JointState)
-			    # message is from arm
-
-			angles = []
-			for name in arm_right_joints:
-				angles.append(jointsMsg.position[jointsMsg.name.index(name)])
-
-			aw_error = self.checks['joint_configuration_check']['arm_right']['allowed_error']
-
-			assert abs(angles[0] - self.checks['joint_configuration_check']['arm_right']['joint_states'][0]) <= aw_error, "Error on the right Arm for the 1-DOF"
-			assert abs(angles[1] - self.checks['joint_configuration_check']['arm_right']['joint_states'][1]) <= aw_error, "Error on the right Arm for the 2-DOF"
-			assert abs(angles[2] - self.checks['joint_configuration_check']['arm_right']['joint_states'][2]) <= aw_error, "Error on the right Arm for the 3-DOF"
-			assert abs(angles[3] - self.checks['joint_configuration_check']['arm_right']['joint_states'][3]) <= aw_error, "Error on the right Arm for the 4-DOF"
-			assert abs(angles[4] - self.checks['joint_configuration_check']['arm_right']['joint_states'][4]) <= aw_error, "Error on the right Arm for the 5-DOF"
-			assert abs(angles[5] - self.checks['joint_configuration_check']['arm_right']['joint_states'][5]) <= aw_error, "Error on the right Arm for the 6-DOF"
-			assert abs(angles[6] - self.checks['joint_configuration_check']['arm_right']['joint_states'][6]) <= aw_error, "Error on the right Arm for the 7-DOF"
+		try:
+			aw_error = self.checks['joint_configuration_check_ss'][0]['allowed_error']
 			
+			for name, state in joints:
 
-		def arm_joint():
-
-			rospy.loginfo("Checking the <<arm>> joints")
-			arm_joints = rospy.get_param("/script_server/arm/joint_names")
-			jointsMsg = rospy.wait_for_message("/joint_states", sensor_msgs.msg.JointState)
-
-			while arm_joints[0] not in jointsMsg.name:
+				rospy.loginfo("Checking the <<%s>> joint"%name)
+				
 				jointsMsg = rospy.wait_for_message("/joint_states", sensor_msgs.msg.JointState)
-			    # message is from arm
 
-			angles = []
-			for name in arm_joints:
-				angles.append(jointsMsg.position[jointsMsg.name.index(name)])
+				while name not in jointsMsg.name:
+					jointsMsg = rospy.wait_for_message("/joint_states", sensor_msgs.msg.JointState)
 
-			aw_error = self.checks['joint_configuration_check']['arm']['allowed_error']
+				value = jointsMsg.position[jointsMsg.name.index(name)]
 
-			assert abs(angles[0] - self.checks['joint_configuration_check']['arm']['joint_states'][0]) <= aw_error, "Error on the Arm for the 1-DOF"
-			assert abs(angles[1] - self.checks['joint_configuration_check']['arm']['joint_states'][1]) <= aw_error, "Error on the Arm for the 2-DOF"
-			assert abs(angles[2] - self.checks['joint_configuration_check']['arm']['joint_states'][2]) <= aw_error, "Error on the Arm for the 3-DOF"
-			assert abs(angles[3] - self.checks['joint_configuration_check']['arm']['joint_states'][3]) <= aw_error, "Error on the Arm for the 4-DOF"
-			assert abs(angles[4] - self.checks['joint_configuration_check']['arm']['joint_states'][4]) <= aw_error, "Error on the Arm for the 5-DOF"
-			assert abs(angles[5] - self.checks['joint_configuration_check']['arm']['joint_states'][5]) <= aw_error, "Error on the Arm for the 6-DOF"
-			assert abs(angles[6] - self.checks['joint_configuration_check']['arm']['joint_states'][6]) <= aw_error, "Error on the Arm for the 7-DOF"
-
-
+				assert abs(value - state) <= aw_error, "Error on the Joint Position for the <<%s>>"%name
 			
-		def torso_joint():
-			aw_error = self.checks['joint_configuration_check']['torso']['allowed_error']
+		except AssertionError,e:
+			self.result = "failed"
+			rospy.logerr("<<Error Message>>:%s"%e)
+			rospy.logerr("at:%s"%check)
+			return
 
-			torso_joints = rospy.get_param("/script_server/torso/joint_names")
-			jointsMsg = rospy.wait_for_message("/joint_states", sensor_msgs.msg.JointState)
-
-			while torso_joints[0] not in jointsMsg.name:
-				jointsMsg = rospy.wait_for_message("/joint_states", sensor_msgs.msg.JointState)
-			    # message is from torso
-
-			angles = []
-			for name in torso_joints:
-				angles.append(jointsMsg.position[jointsMsg.name.index(name)])
-
-			aw_error = self.checks['joint_configuration_check']['torso']['allowed_error']
-	
-			assert abs(angles[0] - self.checks['joint_configuration_check']['torso']['joint_states'][0]) <= aw_error, "Error on the Torso for the 1-DOF"
-			assert abs(angles[1] - self.checks['joint_configuration_check']['torso']['joint_states'][1]) <= aw_error, "Error on the Torso for the 2-DOF"
-			assert abs(angles[2] - self.checks['joint_configuration_check']['torso']['joint_states'][2]) <= aw_error, "Error on the Torso for the 3-DOF"
-			
-
-		for check in self.checks['joint_configuration_check']:
-
-			if self.robot_name == "desire":
-				try:
-					{'arm_left': arm_left_joint, 'arm_right': arm_right_joint, 'torso': torso_joint}[check]()
-
-				except KeyError:
-					pass
-
-				except AssertionError,e:
-					self.result = "failed"
-					rospy.logerr("<<Error Message>>:%s"%e)
-					rospy.logerr("at:%s"%check)
-					return
-			else:
-				try:
-					{'arm': arm_joint, 'torso': torso_joint}[check]()
-
-				except KeyError:
-					pass
-
-				except AssertionError,e:
-					self.result = "failed"
-					rospy.logerr("<<Error Message>>:%s"%e)
-					rospy.logerr("at:%s"%check)
-					return
 
 		self.result = "success"
 
+	####################################################################
+	# function: ready_check()
+	# This function is responsible for checking the initial conditions
+	# provided by the user for proper operation of the Robot
+	#####################################################################
+
+	def ready_check(self): 
+
+		# change The checks for using the diagnostics component
+
+		self.init_components()
+
+	
 	####################################################################
 	# function: pose_check()
 	# This function is responsible for checking if the Robot position
@@ -451,23 +291,20 @@ class PreConditionCheck(ConditionCheck):
 
 	def execute(self, userdata):	
 
-		if self.checks['init_check'] == True:
-			self.init_check()
+		if self.checks['ready_check'] == True:
+			self.ready_check()
 
 		if (self.result == "failed"):
-			rospy.logerr("Check failure on <<init_check>>")
+			rospy.logerr("Check failure on <<ready_check>>")
 			return self.result
 
 		for check in self.checks:
-			try:
-				{'joint_configuration_check': self.joint_configuration_check, 'pose_check': self.pose_check}[check]()
+
+				getattr(self, check)()
 
 				if (self.result == "failed"):
 					rospy.logerr("Check failure on <<%s>>"%check)
 					return self.result
-
-			except KeyError:
-				pass
 
 		# announce ready
 		if ("sound" in self.full_components) and (self.result == "success"):
