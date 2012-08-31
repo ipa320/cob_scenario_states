651c86200f265ed46e822bc862dacbba092cce80
Thiago de Freitas <thiagodefreitas@gmail.com>
2012-08-31 15:13:57 +0200

    Parametrizations and adds checks defined on the new yaml file

--- cob_skill_api/src/pre_check.py
+++ cob_skill_api/src/pre_check.py
@@ -1,6 +1,61 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-# Example of pre-condition processing
+#################################################################
+##\file
+#
+# \note
+# Copyright (c) 2012 \n
+# Fraunhofer Institute for Manufacturing Engineering
+# and Automation (IPA) \n\n
+#
+#################################################################
+#
+# \note
+# Project name: Care-O-bot Research
+# \note
+# ROS package name: cob_skill_api
+#
+# \author
+# Author: Thiago de Freitas Oliveira Araujo, 
+# email:thiago.de.freitas.oliveira.araujo@ipa.fhg.de
+# \author
+# Supervised by: Florian Weisshardt, email:florian.weisshardt@ipa.fhg.de
+#
+# \date Date of creation: August 2012
+#
+# \brief
+# Pre-checks for the Skills API
+#
+#################################################################
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# - Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer. \n
+# - Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution. \n
+# - Neither the name of the Fraunhofer Institute for Manufacturing
+# Engineering and Automation (IPA) nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission. \n
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License LGPL as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License LGPL for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License LGPL along with this program.
+# If not, see < http://www.gnu.org/licenses/>.
+#
+#################################################################
 
 import roslib
 roslib.load_manifest('cob_skill_api')
@@ -9,6 +64,7 @@ import smach
 import smach_ros
 from actionlib import *
 from actionlib.msg import *
+from diagnostic_msgs.msg import DiagnosticArray
 
 import tf
 from tf.msg import tfMessage 
@@ -19,6 +75,7 @@ sss = simple_script_server()
 
 from kinematics_msgs.srv import *
 from sensor_msgs.msg import *
+from move_base_msgs.msg import *
 
 from abc_conditioncheck import ConditionCheck
 
@@ -58,10 +115,33 @@ class PreConditionCheck(ConditionCheck):
 
 		self.result = "failed"
 
-		self.xy_goal_tolerance  = self.checks['pose_check'][0]['allowed_position_error']
-		self.yaw_goal_tolerance = self.checks['pose_check'][0]['allowed_orientation_error']
+		self.status = None
 
+		self.xy_goal_tolerance  = 0.1
+		self.yaw_goal_tolerance = 0.2
 
+		rospy.Subscriber('diagnostics', DiagnosticArray, self.diagnostics_callback)
+
+	####################################################################
+	# function: execute()
+	# Main routine of the State Machine
+	####################################################################
+
+	def execute(self, userdata):	
+
+		for check in self.checks:
+
+				getattr(self, check.keys()[0])(check)
+
+				if (self.result == "failed"):
+					rospy.logerr("Check failure on <<%s>>"%check)
+					return self.result
+
+		# announce ready
+		if ("sound" in self.full_components) and (self.result == "success"):
+			sss.say(["Ready."])
+
+		return self.result
 
 	#####################################################################
 	# function: joint_configuration_check()
@@ -69,33 +149,38 @@ class PreConditionCheck(ConditionCheck):
 	# Joint configuration of the Robot before performing the skill action
 	#####################################################################
 
-	def joint_configuration_check_js(self): # from parameter server, get names and supposed states
+	def joint_configuration_check_js(self, params): # from parameter server, get names and supposed states
 
 		#get joint_names from parameter server (yaml)
 		#get joint_states from parameter server (yaml)
+		aw_error = params.values()[0][0]['allowed_error']
 
 		return self.joint_configuration_check(joint_names,joint_states)
  
 
-	def joint_configuration_check_ss(self):
+	def joint_configuration_check_ss(self, params):
 
-		component = self.checks['joint_configuration_check_ss'][0]['component']
-		configuration = self.checks['joint_configuration_check_ss'][0]['configuration']
+
+
+		component = params.values()[0][0]['component']
+		configuration = params.values()[0][0]['configuration']
 
 		ss_names_path = "/script_server/" + component + "/joint_names"
 		ss_values_path = "/script_server/" + component + "/" + configuration
 
 		joint_names = rospy.get_param(ss_names_path)
 		joint_states = rospy.get_param(ss_values_path)[0]
+		
+		aw_error = params.values()[0][0]['allowed_error']
 
-		return self.joint_configuration_check(joint_names,joint_states)
+		return self.joint_configuration_check(joint_names,joint_states, aw_error)
 
-	def joint_configuration_check(self, joint_names, joint_states):
+	def joint_configuration_check(self, joint_names, joint_states, aw_error):
 
 		joints = zip(joint_names, joint_states)
 
 		try:
-			aw_error = self.checks['joint_configuration_check_ss'][0]['allowed_error']
+			
 			
 			for name, state in joints:
 
@@ -120,16 +205,53 @@ class PreConditionCheck(ConditionCheck):
 		self.result = "success"
 
 	####################################################################
-	# function: ready_check()
+	# function: component_ready_check()
 	# This function is responsible for checking the initial conditions
 	# provided by the user for proper operation of the Robot
 	#####################################################################
 
-	def ready_check(self): 
+	def component_ready_check(self, params): #Simplified due to the current simulation possibilities
 
-		# change The checks for using the diagnostics component
+		try:
+			rospy.loginfo("Started to check if all components are ready")		
 
-		self.init_components()
+			assert self.status == 0, "<<base>> component is not ready yet."
+
+			self.init_components()
+
+			rospy.loginfo("Components successfully initialized.")
+
+		except AssertionError,e:
+			self.result = "failed"
+			rospy.logerr("<<Error Message>>:%s"%e)
+			rospy.logerr("at:%s"%params)
+			return
+
+		self.result = "success"
+
+
+	####################################################################
+	# function: action_check()
+	# This function is responsible for checking the availability of an 
+	# action
+	#####################################################################
+
+
+	def action_check(self, params):
+
+		rospy.loginfo("Checking <<actions>>")
+
+		action_type = params.values()[0][0]['action_type']
+		action_name = params.values()[0][0]['action_name']
+
+		mod = __import__(action_name, fromlist=[action_type])
+		cls = getattr(mod, action_type)
+
+		ac_client = actionlib.SimpleActionClient(action_name, cls)
+
+		ac_client.wait_for_server(rospy.Duration(5))	
+
+		rospy.loginfo("Finished Checking <<actions>>")
 
 	
 	####################################################################
@@ -138,10 +260,10 @@ class PreConditionCheck(ConditionCheck):
 	# is convenient for performing the skill action
 	####################################################################
 
-	def pose_check(self):
+	def pose_check(self, params):
 		
 		try:
-			rospy.loginfo("Checking the Robot Pose")
+			rospy.loginfo("Checking the Robot <<Pose>>")
 
 			self.tfL.waitForTransform("/map", "/base_link", rospy.Time(), rospy.Duration(20.0))
 
@@ -149,81 +271,20 @@ class PreConditionCheck(ConditionCheck):
 
 			angles = euler_from_quaternion(rot)
 		
-			messageX = (str)(trans[0]) + " " + (str)(self.checks['pose_check']['pose'][0]) + " "+ (str)(self.xy_goal_tolerance)
-			messageY = (str)(trans[1]) + " " + (str)(self.checks['pose_check']['pose'][1]) + " "+ (str)(self.xy_goal_tolerance)
-			messageA = (str)(angles[2]) + " " + (str)(self.checks['pose_check']['pose'][2]) + " "+ (str)(self.yaw_goal_tolerance)
+			xy_goal_tolerance = params.values()[0][0]['allowed_position_error']
+			yaw_goal_tolerance = params.values()[0][0]['allowed_orientation_error']
+			
+			messageX = (str)(trans[0]) + " " + (str)(params.values()[0][0]['target_pose']['position'][0]) + " "+ (str)(xy_goal_tolerance)
+			messageY = (str)(trans[1]) + " " + (str)(params.values()[0][0]['target_pose']['position'][1]) + " "+ (str)(xy_goal_tolerance)
+			messageA = (str)(angles[2]) + " " + (str)(params.values()[0][0]['target_pose']['position'][2]) + " "+ (str)(yaw_goal_tolerance)
 
 
-			assert abs(trans[0] - self.checks['pose_check']['pose'][0]) <= self.xy_goal_tolerance, "Error on the X axis position %s"%messageX
-			assert abs(trans[1] - self.checks['pose_check']['pose'][1]) <= self.xy_goal_tolerance, "Error on the Y axis position %s"%messageY
-			assert abs(angles[2] - self.checks['pose_check']['pose'][2]) <= self.yaw_goal_tolerance, "Error on the Angle %s"%messageA
+
+			assert abs(trans[0] - params.values()[0][0]['target_pose']['position'][0]) <= xy_goal_tolerance, "Error on the X axis position %s"%messageX
+			assert abs(trans[1] - params.values()[0][0]['target_pose']['position'][1]) <= xy_goal_tolerance, "Error on the Y axis position %s"%messageY
+			assert abs(angles[2] - params.values()[0][0]['target_pose']['position'][2]) <= yaw_goal_tolerance, "Error on the Angle %s"%messageA
 		
-			if self.robot_name == "desire":
-
-				# Checking Arm 7 Link Position 
-				if "arm_left" in self.full_components:
-					rospy.loginfo("Checking the <<left arm>> Position")
-					now = rospy.Time.now()
-
-					self.tfL.waitForTransform('/base_link', '/arm_left_7_link', now, rospy.Duration(4.0))
-					(trans,rot) = self.tfL.lookupTransform('/base_link', '/arm_left_7_link', now)
-
-					assert abs(trans[0] - self.checks['pose_check']['links']['arm_left_7_link']['position'][0]) <= self.xy_goal_tolerance, "Error on the X axis position for the ARM 7 Link"
-					assert abs(trans[1] - self.checks['pose_check']['links']['arm_left_7_link']['position'][1]) <= self.xy_goal_tolerance, "Error on the Y axis position for the ARM 7 Link"
-					assert abs(trans[2] - self.checks['pose_check']['links']['arm_left_7_link']['position'][2]) <= self.xy_goal_tolerance, "Error on the Z axis position for the ARM 7 Link"
-					assert abs(rot[0] - self.checks['pose_check']['links']['arm_left_7_link']['position'][3]) <= self.yaw_goal_tolerance, "Error on W for the ARM 7 Link"
-					assert abs(rot[1] - self.checks['pose_check']['links']['arm_left_7_link']['position'][4]) <= self.yaw_goal_tolerance, "Error on Raw for the ARM 7 Link"
-					assert abs(rot[2] - self.checks['pose_check']['links']['arm_left_7_link']['position'][5]) <= self.yaw_goal_tolerance, "Error on Pitch for the ARM 7 Link"
-					assert abs(rot[3] - self.checks['pose_check']['links']['arm_left_7_link']['position'][6]) <= self.yaw_goal_tolerance, "Error on Yaw for the ARM 7 Link"
-
-				if "arm_right" in self.full_components:
-					rospy.loginfo("Checking the <<right arm>> Position")
-					now = rospy.Time.now()
-
-					self.tfL.waitForTransform('/base_link', '/arm_right_7_link', now, rospy.Duration(4.0))
-					(trans,rot) = self.tfL.lookupTransform('/base_link', '/arm_right_7_link', now)
-
-					assert abs(trans[0] - self.checks['pose_check']['links']['arm_right_7_link']['position'][0]) <= self.xy_goal_tolerance, "Error on the X axis position for the ARM 7 Link"
-					assert abs(trans[1] - self.checks['pose_check']['links']['arm_right_7_link']['position'][1]) <= self.xy_goal_tolerance, "Error on the Y axis position for the ARM 7 Link"
-					assert abs(trans[2] - self.checks['pose_check']['links']['arm_right_7_link']['position'][2]) <= self.xy_goal_tolerance, "Error on the Z axis position for the ARM 7 Link"
-					assert abs(rot[0] - self.checks['pose_check']['links']['arm_right_7_link']['position'][3]) <= self.yaw_goal_tolerance, "Error on W for the ARM 7 Link"
-					assert abs(rot[1] - self.checks['pose_check']['links']['arm_right_7_link']['position'][4]) <= self.yaw_goal_tolerance, "Error on Raw for the ARM 7 Link"
-					assert abs(rot[2] - self.checks['pose_check']['links']['arm_right_7_link']['position'][5]) <= self.yaw_goal_tolerance, "Error on Pitch for the ARM 7 Link"
-					assert abs(rot[3] - self.checks['pose_check']['links']['arm_right_7_link']['position'][6]) <= self.yaw_goal_tolerance, "Error on Yaw for the ARM 7 Link"
-
-			else:
-				# Checking Arm 7 Link Position 
-				if "arm" in self.full_components:
-					rospy.loginfo("Checking the <<arm>> Position")
-					now = rospy.Time.now()
-
-					self.tfL.waitForTransform('/base_link', '/arm_7_link', now, rospy.Duration(4.0))
-					(trans,rot) = self.tfL.lookupTransform('/base_link', '/arm_7_link', now)
-
-					assert abs(trans[0] - self.checks['pose_check']['links']['arm_7_link']['position'][0]) <= self.xy_goal_tolerance, "Error on the X axis position for the ARM 7 Link"
-					assert abs(trans[1] - self.checks['pose_check']['links']['arm_7_link']['position'][1]) <= self.xy_goal_tolerance, "Error on the Y axis position for the ARM 7 Link"
-					assert abs(trans[2] - self.checks['pose_check']['links']['arm_7_link']['position'][2]) <= self.xy_goal_tolerance, "Error on the Z axis position for the ARM 7 Link"
-					assert abs(rot[0] - self.checks['pose_check']['links']['arm_7_link']['position'][3]) <= self.yaw_goal_tolerance, "Error on W for the ARM 7 Link"
-					assert abs(rot[1] - self.checks['pose_check']['links']['arm_7_link']['position'][4]) <= self.yaw_goal_tolerance, "Error on Raw for the ARM 7 Link"
-					assert abs(rot[2] - self.checks['pose_check']['links']['arm_7_link']['position'][5]) <= self.yaw_goal_tolerance, "Error on Pitch for the ARM 7 Link"
-					assert abs(rot[3] - self.checks['pose_check']['links']['arm_7_link']['position'][6]) <= self.yaw_goal_tolerance, "Error on Yaw for the ARM 7 Link"
-
-			# Checking Head Axis Link Position 
-			if "head" in self.full_components:
-				rospy.loginfo("Checking the <<head>> Position")
-				now = rospy.Time.now()
-
-				self.tfL.waitForTransform('/base_link', '/head_axis_link', now, rospy.Duration(4.0))
-				(trans,rot) = self.tfL.lookupTransform('/base_link', '/head_axis_link', now)
-		
-				assert abs(trans[0] - self.checks['pose_check']['links']['head_axis_link']['position'][0]) <= self.xy_goal_tolerance, "Error on the X axis position for the Head Axis Link"
-				assert abs(trans[1] - self.checks['pose_check']['links']['head_axis_link']['position'][1]) <= self.xy_goal_tolerance, "Error on the Y axis position for the Head Axis Link"
-				assert abs(trans[2] - self.checks['pose_check']['links']['head_axis_link']['position'][2]) <= self.xy_goal_tolerance, "Error on the Z axis position for the Head Axis Link"
-				assert abs(rot[0] - self.checks['pose_check']['links']['head_axis_link']['position'][3]) <= self.yaw_goal_tolerance, "Error on W for the Head Axis Link"
-				assert abs(rot[1] - self.checks['pose_check']['links']['head_axis_link']['position'][4]) <= self.yaw_goal_tolerance, "Error on Raw for the Head Axis Link"
-				assert abs(rot[2] - self.checks['pose_check']['links']['head_axis_link']['position'][5]) <= self.yaw_goal_tolerance, "Error on Pitch for the Head Axis Link"
-				assert abs(rot[3] - self.checks['pose_check']['links']['head_axis_link']['position'][6]) <= self.yaw_goal_tolerance, "Error on Yaw for the Head Axis Link"
-
+			
 		except AssertionError,e:
 			self.result = "failed"
 			rospy.logerr("<<Error Message>>:%s"%e)		
@@ -283,31 +344,7 @@ class PreConditionCheck(ConditionCheck):
 			sss.say(["Ready."])
 
 		sss.sleep(2)
-		
-	####################################################################
-	# function: execute()
-	# Main routine of the State Machine
-	####################################################################
 
-	def execute(self, userdata):	
+	def diagnostics_callback(self, msg):
 
-		if self.checks['ready_check'] == True:
-			self.ready_check()
-
-		if (self.result == "failed"):
-			rospy.logerr("Check failure on <<ready_check>>")
-			return self.result
-
-		for check in self.checks:
-
-				getattr(self, check)()
-
-				if (self.result == "failed"):
-					rospy.logerr("Check failure on <<%s>>"%check)
-					return self.result
-
-		# announce ready
-		if ("sound" in self.full_components) and (self.result == "success"):
-			sss.say(["Ready."])
-
-		return self.result
+		self.status = msg.status[0].level
