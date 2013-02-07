d3321814e6e3e0c9b4706e8bce01fe6e6b090294
Josh <joshua.hampp@ipa.fraunhofer.de>
2013-02-07 03:00:47 -0600

    minor fixes

--- cob_generic_states_experimental/src/BasicIO.py
+++ cob_generic_states_experimental/src/BasicIO.py
@@ -56,7 +56,7 @@
 #################################################################
 
 import roslib
-roslib.load_manifest('cob_generic_states')
+roslib.load_manifest('cob_generic_states_experimental')
 import rospy
 import smach
 import smach_ros
