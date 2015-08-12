d9dd01891559ecece8d4504b25b6b0ff999bf937
Richard Bormann <Richard.Bormann@ipa.fraunhofer.de>
2015-08-12 16:35:47 +0200

    upgraded the package to make scripts visible and usable from external scripts

--- cob_generic_states_experimental/setup.py
+++ cob_generic_states_experimental/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     # #  don't do this unless you want a globally visible script
-    # scripts=['bin/myscript'], 
+    #scripts=['bin/cob_generic_states_experimental'], 
     packages=['cob_generic_states_experimental'],
     package_dir={'': 'src'}
 )
